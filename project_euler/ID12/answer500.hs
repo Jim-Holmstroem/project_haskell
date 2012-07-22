eb885811be1bed3c65424549720092bdc58101a6
Jim Holmström <jim.holmstroem@gmail.com>
2012-07-22 22:32:07 +0200

    removed the things that shouldn't be there

--- project_euler/ID12/answer500.hs
+++
@@ -1,3 +0,0 @@
-divisors :: Integer -> [Integer]
-divisors x = filter (\m->mod x m == 0) $ takeWhile ((<=x).(2*)) [2..] --howto do this without lambda?
-main = print $ head $ filter ((>500).(+2).length.divisors) $ scanl1 (+) [1..] -- +2 comes from the missing nonproper/nontrivial divisors
