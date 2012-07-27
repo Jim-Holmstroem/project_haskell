0854fa629ba08df76e4ab5ee2710dea116fe5533
Jim Holmström <jim.holmstroem@gmail.com>
2012-07-28 00:05:28 +0200

    profit

--- project_euler/Listified.hs
+++ project_euler/Listified.hs
@@ -1,5 +1,11 @@
 
 module Listified
 (
+slidedList
 ) where
+
+import Data.List
+
+-- slides the list with the length l
+slidedList l x = filter ((==l).(length)) $ transpose $ map ($ x) $ map (drop) [0..(l-1)]
 
