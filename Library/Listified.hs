2fc2f73026769de18cc38ab0dff411d780c2ec0b
Jim Holmström <jim.holmstroem@gmail.com>
2012-07-30 06:55:04 +0200

    added testmatrix to library

--- Library/Listified.hs
+++ Library/Listified.hs
@@ -1,11 +1,16 @@
 
 module Listified
-(
-slidedList
+(slidedList
+,testmatrix
+--,printMatrix
 ) where
 
 import Data.List
 
 -- slides the list with the length l
 slidedList l x = filter ((==l).(length)) $ transpose $ map ($ x) $ map (drop) [0..(l-1)]
+
+testmatrix = [[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
+--printMatrix::[a]->IO()
+--printMatrix = putStr.unlines.(map (show)) --for some reason doesn't work copy it
 
