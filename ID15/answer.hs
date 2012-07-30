29ffbc04fd9e4d91690c3b97ee8bf6ec9c4d9e71
Jim Holmström <jim.holmstroem@gmail.com>
2012-07-30 07:41:17 +0200

    started on ID15, realized it was harder then I first thought, maybe I need to use young diagrams ?

--- ID15/answer.hs
+++ ID15/answer.hs
@@ -1,1 +1,4 @@
-main = putStr $ "Hello Haskell\n"
+
+-- Reduce the problem to "on which line do I go down" and represent those as a binary number. We need to have atleast one place which it goes down to be able to reach the bottom and additionally it can't go down on each line since it would render the path outside the rectangle.
+width = 20
+main = print $ 2^(width+1)-2
