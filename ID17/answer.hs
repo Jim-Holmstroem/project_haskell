f970e672b30c65bb6e1618ffcbf83df95a77a7fc
Jim Holmström <jim.holmstroem@gmail.com>
2012-07-31 07:37:46 +0200

    f

--- ID17/answer.hs
+++ ID17/answer.hs
@@ -1,28 +1,33 @@
 
 import Digit
 
-zero2nine    =[4,3,3,5,4,4,3,5,5,4]
-ten2nineteen =[3,6,6,8,8,7,7,9,9,8]
+-- Using letters and then count them to get it easier to debug
+
+zero2nine    =["","one","two","three","four","five","six","seven","eight","nine"] -- you never say zero in counting except for zero
+ten2nineteen =["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+
+
 
 -- as a function to avoid
-tens::Int->Int
+tens::Int->String
 tens c
-    | c == 2 = 6
-    | c == 3 = 6
-    | c == 4 = 5
-    | c == 5 = 5
-    | c == 6 = 5
-    | c == 7 = 7
-    | c == 8 = 6
-    | c == 9 = 6
+    | c == 2 = "twenty"
+    | c == 3 = "thirty"
+    | c == 4 = "fourty"
+    | c == 5 = "fifty"
+    | c == 6 = "sixty"
+    | c == 7 = "seventy"
+    | c == 8 = "eighty"
+    | c == 9 = "ninety"
     | otherwise = error "that tens is out of range"
 
 hundred = 7
 thousand = 8
 andc = 3
 
-numberOfLetters::Int->Int
+numberOfLetters::Int->String
 numberOfLetters c
+    | c == 0                        = error "zero permitted"
     | c < 10 = zero2nine!!c -- [0..9]
     | 10 <= c && c < 20             = ten2nineteen!!(c-10) -- [10..19]
     | c<100 && (mod c 10)==0        = tens (div c 10) -- [20,30..90]
@@ -33,4 +38,4 @@ numberOfLetters c
 
     | otherwise                     = error "Range not implemented"
 
-main = print .(!!(1000-1)) . (map (numberOfLetters)) $ [1..1000]
+main = print . sum. (map (numberOfLetters)) $ [1..1000]
