bff8db13f22d5d244ecffbb589e13b102cbb4ae1
Jim Holmström <jim.holmstroem@gmail.com>
2012-08-02 20:50:39 +0200

    solved 25

--- ID25/answer.hs
+++ ID25/answer.hs
@@ -2,5 +2,5 @@ import Digit
 
 fibs = 1:2:zipWith (+) fibs (tail fibs)
 
--- +1 for the missing starting F_1
-main = print . (+1) .length . (takeWhile  ((<=1000).(length).(digits)) ) $ fibs
+-- +1 for the missing starting F_1 (and +1 agains since the actual fibonccinumbers is just almost picked out (next in line))
+main = print . (+1) . (+1) . length . (takeWhile  ((<1000).(length).(digits)) ) $ fibs
