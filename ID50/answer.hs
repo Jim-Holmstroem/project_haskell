a3560ad5f7074f4ed82cd4ff7e83a2b7151ee88f
Jim Holmström <jim.holmstroem@gmail.com>
2012-08-04 12:15:27 +0200

    solved it but it takes alot of ram

--- ID50/answer.hs
+++ ID50/answer.hs
@@ -1,12 +1,13 @@
 
 import Prime
 import Listified
+import Data.List
 
 
 n=1000000
 --MISS (old code): the problem where the consecutive primes has to start at 2 is too easy
 
+-- <div n 2 is under the assumption that it exists some sequence bigger then 2 (which is true for n larger then 
 
-main = print . continousSubSeq 
---main = print . reverse . (takeWhile (<n)) .(scanl1 (+)) $ primes
---main = print . (filter isPrime).(scanl1 (+)) $ primes
+main = print . (take 1) . (filter isPrime) . (filter (<n)) . (map sum) . reverse . (sortBy (\x y->(length x) `compare` (length y))) . continousSubSeqs . (takeWhile (<(div n 2))) $ primes
+
