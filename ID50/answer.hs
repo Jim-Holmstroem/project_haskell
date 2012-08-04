5eceb2e2dac3b192140d543a77d0837846b07ef9
Jim Holmström <jim.holmstroem@gmail.com>
2012-08-04 21:09:01 +0200

    f

--- ID50/answer.hs
+++ ID50/answer.hs
@@ -4,10 +4,13 @@ import Listified
 import Data.List
 
 
-n=1000000
+n=100
 --MISS (old code): the problem where the consecutive primes has to start at 2 is too easy
+--First version uses to much RAM (15G+), must generate the subsequence in order, cant make them out of order and then sort them.
 
--- <div n 2 is under the assumption that it exists some sequence bigger then 2 (which is true for n larger then 
+--main = print.(take 1).(filter isPrime).(filter (<n)).(map sum).reverse.continousSubSeqs.(takeWhile (<(div n 16))) $ primes
 
-main = print . (take 1) . (filter isPrime) . (filter (<n)) . (map sum) . reverse . (sortBy (\x y->(length x) `compare` (length y))) . continousSubSeqs . (takeWhile (<(div n 2))) $ primes
+--the assumption that n > 1000 which makes the sequence at least 21 (as in the example)
+availablePrimes = (takeWhile < div n 20) $ primes
 
+main = print.(take 1).(filter isPrime).(filter (<n)).(map sum).
