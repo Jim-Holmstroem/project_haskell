5a2e5fa679e0d942a38f07a297b42fbb9c00e6fa
Jim Holmström <jim.holmstroem@gmail.com>
2012-08-04 03:16:41 +0200

    i have the answer new, it should be ANY cont. subseq not necessarly starting at 2

--- ID50/answer.hs
+++ ID50/answer.hs
@@ -1,6 +1,12 @@
 
 import Prime
+import Listified
+
 
 n=1000000
-main = print . head . (filter isPrime) . reverse . (takeWhile (<n)) . (scanl1 (+)) $ primes
+--MISS (old code): the problem where the consecutive primes has to start at 2 is too easy
 
+
+main = print . continousSubSeq 
+--main = print . reverse . (takeWhile (<n)) .(scanl1 (+)) $ primes
+--main = print . (filter isPrime).(scanl1 (+)) $ primes
