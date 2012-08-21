9051bb5c6a4f8784d5edf66c2cff39d2890f6466
Jim Holmstroem <jim.holmstroem@gmail.com>
2012-08-21 21:06:40 +0200

    solved ID21

--- ID21/answer.hs
+++ ID21/answer.hs
@@ -1,1 +1,10 @@
-main = putStr $ "Hello Haskell\n"
+import NumberTheory
+
+-- d(a) = b, d(b) = a, a/=b <==>
+-- d(a) /= a, d^2(a) = a 
+
+d = sum . properDivisors
+
+amicable = (filter (\a->(d.d$a)==a)).(filter (\a->(d a)/=a))
+
+main = print . sum . amicable $ [1..(10000-1)]
