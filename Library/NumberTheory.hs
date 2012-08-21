7719335e4ace97ce5b0ee90bcab128372bd14a91
Jim Holmstroem <jim.holmstroem@gmail.com>
2012-08-21 20:42:21 +0200

    added properDivisors in library

--- Library/NumberTheory.hs
+++ Library/NumberTheory.hs
@@ -16,4 +16,5 @@ listlcm = foldl1 (lcm)
 nCr::Integer->Integer->Integer
 nCr n r = div (product [(n-r+1)..n]) (product [2..r]) 
 
-
+properDivisors::Integer->[Integer]
+properDivisors n = filter ((==0).(n `mod`)) $ [1..(div n 2)]
