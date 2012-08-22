193fb977ce5ac2c43d108e1a7e46d000d83e4df2
Jim Holmstroem <jim.holmstroem@gmail.com>
2012-08-23 00:00:21 +0200

    added isPerfectNumber and the </> versions

--- Library/NumberTheory.hs
+++ Library/NumberTheory.hs
@@ -4,6 +4,10 @@ module NumberTheory
 , listlcm
 , nCr
 , properDivisors
+, amicable
+, isPerfectNumber
+, isDeficientNumber
+, isAbundantNumber
 ) where
 
 -- gcd on a list
@@ -19,3 +23,15 @@ nCr n r = div (product [(n-r+1)..n]) (product [2..r])
 
 properDivisors::Integer->[Integer]
 properDivisors n = filter ((==0).(n `mod`)) $ [1..(div n 2)]
+
+d = sum . properDivisors
+
+amicable = (filter (\a->(d.d$a)==a)).(filter (\a->(d a)/=a))
+
+isPerfectNumber::Integer->Bool
+isPerfectNumber = \n->(d n)==n
+isDeficientNumber::Integer->Bool
+isDeficientNumber = \n->(d n)<n
+isAbundantNumber::Integer->Bool
+isAbundantNumber = \n->(d n)>n
+
