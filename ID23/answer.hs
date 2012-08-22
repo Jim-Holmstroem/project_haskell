5a87c9c578d22addc9b3c6770ff64e5fe507ae43
Jim Holmstroem <jim.holmstroem@gmail.com>
2012-08-23 00:01:01 +0200

    tried bruteforce, too long execution dough

--- ID23/answer.hs
+++ ID23/answer.hs
@@ -1,1 +1,14 @@
-main = putStr $ "Hello Haskell\n"
+import NumberTheory
+import List
+
+-- 28123, provable that it doesnt exist larger numbers then this.
+
+-- addition is commutative
+
+maximal = 28123
+
+possible = (filter isAbundantNumber) $ [1..(div maximal 2)] --since the maximal used can only be maximal/2+maimal/2 (or smaller)
+
+main = print.sum.nub $ [a+b|a<-possible,b<-possible,b<=a]
+
+
