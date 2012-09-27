0b33e690e4bf3ae3e10f064869268a1a00a92fb2
Jim Holmström <jim.holmstroem@gmail.com>
2012-09-27 23:18:30 +0200

    more on the suduko solver

--- ID96/answer.hs
+++ ID96/answer.hs
@@ -1,8 +1,13 @@
+import Data.List
 
-import System.IO
+test_suduko = [[0,0,3,0,2,0,6,0,0],[9,0,0,3,0,5,0,0,1],[0,0,1,8,0,6,4,0,0],[0,0,8,1,0,2,9,0,0],[7,0,0,0,0,0,0,0,8],[0,0,6,7,0,8,2,0,0],[0,0,2,6,0,9,5,0,0],[8,0,0,2,0,3,0,0,9],[0,0,5,0,1,0,3,0,0]]
 
+takenh = (take 9).repeat -- The zero is just ignored later
+takenv = transpose.takenh.transpose
+--takenb = --taken in the box
 
+omega = [1..9]
 
-main = do
-    contents <- getContents
-    print contents
+numbers_left suduko = map $ map $ (\\) omega  (union takenh(suduko) takenv(suduko))
+
+main = print.numbers_left $ test_suduko
