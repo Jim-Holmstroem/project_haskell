f74f8ed07f549f9bc613974e2bd55deaf3d69449
Jim Holmström <jim.holmstroem@gmail.com>
2013-04-03 08:52:46 +0200

    moar progress, but still way too slow

--- ID78/answer.hs
+++ ID78/answer.hs
@@ -10,15 +10,16 @@ p :: Int -> Int
 p 0 = 1
 p 1 = 1
 p 2 = 2
-p 3 = 3
-p 4 = 5
-p 5 = 7
 p n = sum.(map (pk n)) $ [1..n] --sum up all the partitions with [1..n] parts
 
 --Should be possible with dynamic programming, since after choosing the first
 --split we have a smaller subproblem already calculated.
 --
 --The problem can be reformulated as counting the possible partitions of an integer n
+--
+--The bruteforce calculation (using memoziation) is too slow (30min+ and p(n)>4800000000)
+--
+--Thought 1: rewrite p(n) using the recurrence for p_k(n)
 --
 
 pk :: Int -> Int -> Int
@@ -32,4 +33,9 @@ pk n k -- n,k>=2
     | otherwise             = ( pk (n-1) (k-1) ) + ( pk (n-k) k )
 --Haskell => Memoized => Dynamic programming
 
-main = (mapM_ print) $ map p $ [1..100]
+divisible :: Int -> Int -> Bool
+divisible n = (==0).(flip rem n)
+
+--main = print $ head.filter (divisible 1000000) $ map p [1..]
+main = (mapM_ print) $ map p [1..]
+
