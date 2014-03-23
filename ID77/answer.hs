fa3c530d80eeee57e263aeaadba31816b9f336e5
Jim Holmström <jim.holmstroem@gmail.com>
2014-03-24 00:58:59 +0100

    Stylistic changes and explicit typedefs

--- ID77/answer.hs
+++ ID77/answer.hs
@@ -1,10 +1,12 @@
 import qualified Prime (primes, isPrime)
 
 
+primes_up_to :: Int -> [Int]
 primes_up_to k = takeWhile (<=k) Prime.primes
 
 
 -- Number of splits with maximum of k summing up to n
+n_splits_up_to_k :: Int -> Int -> Int
 n_splits_up_to_k n k
     | n == 0 = 1 -- valid combination (k always prime if this is true
     | n < 2 = 0 -- not a valid combination
@@ -13,13 +15,15 @@ n_splits_up_to_k n k
 
 
 -- Number of splits of n
+n_splits :: Int -> Int
 n_splits 0 = 0
 n_splits 1 = 0
 n_splits 2 = 1
 n_splits n = n_splits_up_to_k n n
 
-
+condition :: Int -> Bool
 condition = (>5000).n_splits
 --debug = (mapM (\x->print (x,n_splits x))) $ [1..]
 --main = debug
+main :: IO ()
 main = print.head.(filter condition) $ [2..]
--- Library/Prime.hs
+++ Library/Prime.hs
@@ -1,39 +1,38 @@
 
-module Prime 
+module Prime
 ( primeFactors
 , primes
 , isPrime
-) where 
+) where
 
-
-primeFactors :: Integer -> [Integer]
+primeFactors :: Int -> [Int]
 primeFactors n = factor primes n
-  where 
-    factor ps@(p:pt) n | p*p > n      = [n]               
-                       | rem n p == 0 = p : factor ps (quot n p) 
+  where
+    factor ps@(p:pt) n | p*p > n      = [n]
+                       | rem n p == 0 = p : factor ps (quot n p)
                        | otherwise    =     factor pt n
     -- primes = 2 : filter (\n-> n==head(factor primes n)) [3,5..]
     -- primes = 2 : filter isPrime [3,5..]      -- isPrime of Q.31
     primes = primesTME
 
 -- duplicates-removing union of two ordered increasing lists
-union (x:xs) (y:ys) = case (compare x y) of 
+union (x:xs) (y:ys) = case (compare x y) of
            LT -> x : union  xs  (y:ys)
-           EQ -> x : union  xs     ys 
+           EQ -> x : union  xs     ys
            GT -> y : union (x:xs)  ys
 
 {-# OPTIONS_GHC -O2 -fno-cse #-}
 -- tree-merging Eratosthenes sieve
 --  producing infinite list of all prime numbers
+primesTME :: [Int]
 primesTME = 2 : gaps 3 (join [[p*p,p*p+2*p..] | p <- primes'])
   where
     primes' = 3 : gaps 5 (join [[p*p,p*p+2*p..] | p <- primes'])
     join  ((x:xs):t)        = x : union xs (join (pairs t))
     pairs ((x:xs):ys:t)     = (x : union xs ys) : pairs t
-    gaps k xs@(x:t) | k==x  = gaps (k+2) t 
+    gaps k xs@(x:t) | k==x  = gaps (k+2) t
                     | True  = k : gaps (k+2) xs
 
 primes = primesTME
 
 isPrime = (==1).(length).(primeFactors)
-
