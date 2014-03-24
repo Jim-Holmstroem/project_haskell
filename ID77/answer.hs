b398f92bd0f3245ec7e06e4a86ba9473ad92282a
Jim Holmström <jim.holmstroem@gmail.com>
2014-03-24 01:09:40 +0100

    DOC

--- ID77/answer.hs
+++ ID77/answer.hs
@@ -1,5 +1,14 @@
 import qualified Prime (primes, isPrime)
 
+-- splits 5 = splits_up_to_k 5 5
+-- denote it with 5| resp. 5|5
+--
+-- Decompose to all possible splits
+-- 5|5 = 0|5 + 2|3 + 3|2 = 0|5 + (0|3 + -1|2) + 1|2
+--
+-- Evaluate the number of splits for the decomposition (n_splits_up_to_k)
+-- #{5|5} = #{0|5} + #{2|3} + #{3|2} = #{0|5} + (#{0|3} + #{-1|2}) + #{1|2} =
+-- = 1 + (1 + 0) + 0 = 2
 
 primes_up_to :: Int -> [Int]
 primes_up_to k = takeWhile (<=k) Prime.primes
